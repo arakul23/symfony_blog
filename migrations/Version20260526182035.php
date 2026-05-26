ea779dede26b38473f59746a8b3fbcef51c0d316
Andriy Kuznetsov <andriy.kuznetsov@playwing.net>
2026-05-26 21:56:04 +0300

    .

---
+++ migrations/Version20260526182035.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260526182035 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE blog (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, text LONGTEXT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('DROP TABLE users');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE users (id BINARY(16) NOT NULL, login VARCHAR(8) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, phone VARCHAR(8) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, role INT NOT NULL, token LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX uniq_login (login), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE blog');
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DefaultController extends AbstractController
 {
-    #[Route('/default/{id}', name: 'app_default')]
-    public function index(int $id): Response
+    #[Route('/default/{id}', name: 'app_default', defaults: ['id' => 'index'])]
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        dd($id);
+      $blog = new Blog()->setTitle('test')->setText('test');
+
+      $entityManager->persist($blog);
+      $entityManager->flush($blog);
         return $this->render('index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
